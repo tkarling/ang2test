9175a4a86671a0f3c6a5cb98e0a6e51d6d838b3f
tkarling <tkarling@live.com>
2015-08-09 20:19:23 -0600

    added routing & todo-list page

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var router_1 = require('angular2/router');
+var TodoListComponent = (function () {
+    function TodoListComponent() {
+    } // public cartService:CartService
+    TodoListComponent = __decorate([
+        angular2_1.Component({ selector: "todo-list" }),
+        angular2_1.View({
+            templateUrl: "templates/todo-list.html"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TodoListComponent);
+    return TodoListComponent;
+})();
 var FriendsService = (function () {
     function FriendsService() {
         //this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
@@ -20,33 +33,52 @@ var FriendsService = (function () {
     };
     return FriendsService;
 })();
-var MyAppComponent = (function () {
-    function MyAppComponent(friendsService) {
+var MyFriendsComponent = (function () {
+    function MyFriendsComponent(friendsService) {
         this.name = 'Alice';
-        //this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
         this.friendsService = friendsService;
         this.names = friendsService.names;
     }
-    MyAppComponent.prototype.addFriend = function (friend) {
+    MyFriendsComponent.prototype.addFriend = function (friend) {
         this.friendsService.addFriend(friend);
     };
-    MyAppComponent.prototype.doneTyping = function ($event) {
+    MyFriendsComponent.prototype.doneTyping = function ($event) {
         if ($event.which === 13) {
             this.addFriend($event.target.value);
             $event.target.value = null;
         }
     };
-    MyAppComponent = __decorate([
+    MyFriendsComponent = __decorate([
         angular2_1.Component({
-            selector: 'my-app',
+            selector: 'my-friends',
             appInjector: [FriendsService]
         }),
         angular2_1.View({
-            template: "\n      <p>My name: {{ name }}</p>\n      <input #newfriend (keyup)=\"doneTyping($event)\">\n      <button (click)=\"addFriend(newfriend.value); newfriend.value = null\">add friend</button>\n      <p>Friends:</p>\n      <ul>\n         <li *ng-for=\"#name of names\">\n            {{ name }}\n         </li>\n      </ul>\n      <p *ng-if=\"names.length > 3\">You have many friends</p>\n    ",
+            templateUrl: "templates/friends-list.html",
             directives: [angular2_1.NgFor, angular2_1.NgIf]
         }), 
         __metadata('design:paramtypes', [FriendsService])
+    ], MyFriendsComponent);
+    return MyFriendsComponent;
+})();
+var MyAppComponent = (function () {
+    function MyAppComponent() {
+    }
+    MyAppComponent = __decorate([
+        router_1.RouteConfig([
+            { path: "/", as: "home", component: MyFriendsComponent },
+            { path: "/todo", as: "todo", component: TodoListComponent },
+        ]),
+        angular2_1.Component({
+            appInjector: [router_1.routerInjectables],
+            selector: "my-app"
+        }),
+        angular2_1.View({
+            directives: [router_1.RouterOutlet, router_1.RouterLink],
+            templateUrl: "templates/my-app.html"
+        }), 
+        __metadata('design:paramtypes', [])
     ], MyAppComponent);
     return MyAppComponent;
 })();
-angular2_1.bootstrap(MyAppComponent);
+angular2_1.bootstrap(MyAppComponent, [FriendsService]).then(function (success) { return console.log("success: ", success); }, function (error) { return console.log("error: ", error); });
--- app.ts
+++ app.ts
@@ -1,4 +1,14 @@
 import {Component, View, bootstrap, NgFor, NgIf} from 'angular2/angular2';
+import {RouteConfig, RouterOutlet, RouterLink, Router, routerInjectables} from 'angular2/router';
+
+@Component({selector:"todo-list"})
+@View({
+    templateUrl:"templates/todo-list.html"
+    //directives: [NgFor]
+})
+class TodoListComponent {
+    constructor(){} // public cartService:CartService
+}
 
 class FriendsService {
     names: Array<string>;
@@ -12,32 +22,20 @@ class FriendsService {
 }
 
 @Component({
-    selector: 'my-app',
+    selector: 'my-friends',
     appInjector: [FriendsService]
 })
 @View({
-    template: `
-      <p>My name: {{ name }}</p>
-      <input #newfriend (keyup)="doneTyping($event)">
-      <button (click)="addFriend(newfriend.value); newfriend.value = null">add friend</button>
-      <p>Friends:</p>
-      <ul>
-         <li *ng-for="#name of names">
-            {{ name }}
-         </li>
-      </ul>
-      <p *ng-if="names.length > 3">You have many friends</p>
-    `,
+    templateUrl:"templates/friends-list.html",
     directives: [NgFor, NgIf]
 })
 
-class MyAppComponent {
+class MyFriendsComponent {
     name:string;
     names:Array<string>;
 
     constructor(friendsService: FriendsService) {
         this.name = 'Alice';
-        //this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
         this.friendsService = friendsService;
         this.names = friendsService.names;
     }
@@ -54,5 +52,22 @@ class MyAppComponent {
     }
 }
 
+@RouteConfig([
+    {path:"/", as:"home", component:MyFriendsComponent},
+    {path:"/todo", as:"todo", component:TodoListComponent},
+])
+@Component({
+    appInjector:[routerInjectables],
+    selector:"my-app"
+})
+@View({
+    directives: [RouterOutlet, RouterLink],
+    templateUrl:`templates/my-app.html`
+})
+class MyAppComponent{}
 
-bootstrap(MyAppComponent);
+
+bootstrap(MyAppComponent, [FriendsService]).then(
+        success => console.log("success: ", success),
+        error => console.log("error: ", error)
+);
